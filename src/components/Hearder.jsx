bcc8fc74420552be153ecbe364fcff6a85268707
Rafael <contato.rafaelgomes@outlook.com>
2024-03-01 14:16:27 -0300

    stylize header component

--- src/components/Hearder.jsx
+++ src/components/Hearder.jsx
@@ -1,4 +1,9 @@
 import styles from "./Header.module.css";
+import igniteLogo from "../assets/ignite-logo.svg";
 export const Header = () => {
-  return <strong className={styles.header}>Ignite Feed</strong>;
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="logotipo do ignite" />
+    </header>
+  );
 };
