956b073d6b7e1226db68ade9442158c0f7574b93
Rafael <contato.rafaelgomes@outlook.com>
2024-03-01 13:39:53 -0300

    use css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Post } from "./Post";
+import "./styles.css";
+import { Header } from "./components/Hearder";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post content="Jhon Snow" />
       <Post content="Aria Stark" />
       <Post content="Jamie Lannister" />
